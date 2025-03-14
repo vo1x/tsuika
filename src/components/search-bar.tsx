d19e9f3a64464d951dd8afa8d9e2b15b08299aad
vo1x <ak2061@icloud.com>
2025-03-14 12:31:29 +0545

    update: fix types

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,6 +1,10 @@
 import { Search } from "lucide-react";
+import { ChangeEvent } from "react";
 
-export const SearchBar = ({ setSearchTerm, searchTerm }) => {
+export const SearchBar: React.FC<{
+  setSearchTerm: (value: string) => void;
+  searchTerm: string;
+}> = ({ setSearchTerm, searchTerm }) => {
   return (
     <div className="flex max-w-xl w-full relative">
       <div className="absolute left-3 top-1/2 transform -translate-y-1/2 pointer-events-none">
@@ -9,7 +13,9 @@ export const SearchBar = ({ setSearchTerm, searchTerm }) => {
       <input
         type="text"
         value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setSearchTerm(e.target.value)
+        }
         className="w-full py-2.5 pl-10 pr-4 bg-rosePine-surface border border-rosePine-highlight-med rounded-lg 
                  focus:outline-none focus:border-rosePine-iris
                  placeholder-rosePine-subtle text-rosePine-text transition-colors"
